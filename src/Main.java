320d5986aa070e3a8f73ae4074b3150ce62bc81a
federicomaniglio <fmaniglio@itisrossi.vi.it>
2023-12-05 11:07:35 +0100

    WebScraper V1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Arcaro stai attento");
+        webScraper();
+    }
+
+    public static void webScraper() {
+        Connection conn = Jsoup.connect("https://www.imdb.com/chart/moviemeter/");
+        Document doc;
+        try {
+            doc = conn.get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    int counter = 0;
+        for (Element ul : doc.select("ul.ipc-metadata-list.ipc-metadata-list--dividers-between.sc-9d2f6de0-0.iMNUXk.compact-list-view.ipc-metadata-list--base")) {
+            for (Element title : ul.select("h3.ipc-title__text")) {
+                counter++;
+                System.out.println(counter + " " + title.text());
+            }
+        }
     }
 }
